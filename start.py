795f5b3bbaaee457d212fb98866ddb9543e72965
root <root@NB1936.inside.glowbyte.com>
2022-12-05 10:32:58 +0300

    05_12_22_my_branch

--- start.py
+++ start.py
@@ -5,4 +5,5 @@ python_path, user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to
 
 if __name__ == '__main__':
     test = Teamcity(user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, schema)
-    test.start()
+    #test.start()
+    print(test.get_env_variable('echo $(pwd)'))
