9374a965f7976b23277071f5b3997788a7af2331
Igor21209 <somebody73296@mail.ru>
2022-12-16 19:40:34 +0300

    последняя версия на 16.12.22 19:40

--- teamcity.py
+++ teamcity.py
@@ -7,6 +7,7 @@ import sys
 import tempfile
 from dataclasses import dataclass, field
 from datetime import datetime
+import os
 
 
 @dataclass
@@ -17,7 +18,7 @@ class Commit:
 
 
 class Teamcity:
-    def __init__(self, user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, oracle_user):
+    def __init__(self, user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, oracle_user, oracle_port):
         self.user = user
         self.host = host
         self.target_dir = target_dir
@@ -27,10 +28,11 @@ class Teamcity:
         self.oracle_host = oracle_host
         self.oracle_db = oracle_db
         self.oracle_user = oracle_user
+        self.oracle_port = oracle_port
 
     def runSqlQuery(self, sqlCommand):
         session = Popen([f'{self.path_to_sqlplus}', '-S',
-                         f'{self.oracle_user}/{self.get_env_variable("echo $PASS")}@//{self.oracle_host}:1521/{self.oracle_db}'], stdin=PIPE, stdout=PIPE,
+                         f'{self.oracle_user}/{os.environ.get("PASS")}@//{self.oracle_host}:{self.oracle_port}/{self.oracle_db}'], stdin=PIPE, stdout=PIPE,
                         stderr=PIPE)
         session.stdin.write(sqlCommand)
         if session.communicate():
@@ -40,14 +42,6 @@ class Teamcity:
             if unknown_command:
                 sys.exit(f'Error while executing sql code in file {sqlCommand}')
         return session.communicate()
-
-    def get_env_variable(self, command):
-        process = Popen(
-            args=command,
-            stdout=PIPE,
-            shell=True
-        )
-        return process.communicate()[0].decode('UTF-8').strip()
 
     def yaml_parser(self, path):
         with open(f'{path}', 'r') as f:
@@ -66,6 +60,8 @@ class Teamcity:
     def check_incorrect_order(self, commits_array, branch_array):
         patch_index = 0
         result_compare_order = False
+        if len(branch_array) == 0:
+            return False
         if len(commits_array) < len(branch_array):
             return True
         while branch_array[0] != commits_array[patch_index].branch and patch_index < len(commits_array):
@@ -80,13 +76,19 @@ class Teamcity:
             patch_index += 1
         return result_compare_order
 
+    def get_current_branch(self):
+        current_branch = self.run_shell_command('git branch --show-current').strip()
+        return current_branch
+
     def execute_files(self, patches):
         patches_1 = patches.get('patch')
         patches_for_install = self.get_patches_for_install(patches_1)
+        if len(patches_for_install) == 0:
+            sys.exit(f'Nothing to install')
         patches_for_install_order = self.check_patches(patches_1, patches_for_install)
         list_of_commit_objects = self.git(patches_for_install)
         check = self.check_incorrect_order(list_of_commit_objects, patches_for_install_order)
-        if check:
+        if not check:
             for patch in list_of_commit_objects:
                 pars = f'Patches/{patch.branch}/deploy.yml'
                 data = self.yaml_parser(pars)
@@ -99,8 +101,18 @@ class Teamcity:
                 if sas:
                     for s in sas:
                         self.ssh_copy(s, self.target_dir)
+                add_to_install_patches = f"""whenever sqlerror exit sql.sqlcode
+MERGE INTO PATCH_STATUS USING DUAL ON (PATCH_NAME = '{patch.branch}')
+WHEN NOT MATCHED THEN INSERT (PATCH_NAME, INSTALL_DATE, STATUS)
+VALUES('{patch.branch}', current_timestamp, 'SUCCESS')
+WHEN MATCHED THEN UPDATE SET INSTALL_DATE=current_timestamp, STATUS='SUCCESS';
+exit;"""
+                with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
+                    fp.write(add_to_install_patches)
+                    fp.flush()
+                    self.runSqlQuery(bytes(f"@{fp.name}", 'UTF-8'))
         else:
-            sys.exit(f'Some problem with patch')
+            sys.exit(f"Patches order does not match commits order")
 
     def ssh_copy(self, sourse, target):
         dirs = re.split('/', sourse)
@@ -142,9 +154,6 @@ class Teamcity:
         sql_command = sql_exec.communicate()[0]
         return sql_command
 
-    def sort(self, date):
-        return date.date
-
     def git(self, patches):
         commit_list = []
         for patch_name in patches:
@@ -154,48 +163,43 @@ class Teamcity:
             for commit in list_of_commits:
                 branch = f'git show {commit}'
                 get_branch = self.run_shell_command(branch)
-                branch_1 = re.search('Merge: .+ (.+)', get_branch).group(1)
-                get_branch_1 = self.run_shell_command(f'git show  {branch_1}')
-                commit_version = re.search('commit (.+)', get_branch_1).group(1)
-                date = re.search('Date: (.+)', get_branch_1).group(1).strip()
-                name_of_branch = self.run_shell_command(f'git name-rev {branch_1}')
-                branch_name = re.search('.+ (.+)', name_of_branch).group(1)
+                date = re.search('Date: (.+)', get_branch).group(1).strip()
+                branch_name = re.search('\{\%(.+)\%\}', get_branch).group(1)
                 if branch_name == patch_name:
-                    commit_list.append(Commit(commit_version, date, branch_name))
-        commit_list.sort(reverse=False, key=self.sort)
-        print(commit_list)
+                    commit_list.append(Commit(commit, date, branch_name))
+        commit_list.sort(reverse=False, key=lambda comm: comm.date)
         return commit_list
 
     def get_patches_for_install(self, patches):
         patches_for_install = []
-        query_1 = "whenever sqlerror exit sql.sqlcode\
-        \nCREATE OR REPLACE TYPE arr_patch_type IS TABLE OF VARCHAR2(32);\
-        \n/\
-        \nexit;"
+        query_1 = """whenever sqlerror exit sql.sqlcode
+CREATE OR REPLACE TYPE arr_patch_type IS TABLE OF VARCHAR2(32);
+/
+exit;"""
         with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
             fp.write(query_1)
-            fp.seek(0)
+            fp.flush()
             self.runSqlQuery(bytes(f"@{fp.name}", 'UTF-8'))
         deploy_order = str(patches).replace('[', '(').replace(']', ')').strip()
-        query_2 = f"SET SERVEROUTPUT ON\
-        \nwhenever sqlerror exit sql.sqlcode\
-        \nDECLARE\
-        \nall_patches_list arr_patch_type := arr_patch_type{deploy_order};\
-        \nuninstalled_patches arr_patch_type := arr_patch_type();\
-        \ninstalled_patches arr_patch_type := arr_patch_type();\
-        \nBEGIN\
-        \nSELECT PATCH_NAME BULK COLLECT INTO installed_patches FROM PATCH_STATUS\
-        \nWHERE PATCH_NAME IN (select * from table(all_patches_list));\
-        \nuninstalled_patches := all_patches_list MULTISET EXCEPT installed_patches;\
-        \nFOR i IN 1..uninstalled_patches.COUNT LOOP\
-        \nDBMS_OUTPUT.PUT_LINE(uninstalled_patches(i));\
-        \nEND LOOP;\
-        \nEND;\
-        \n/\
-        \nexit;"
+        query_2 = f"""SET SERVEROUTPUT ON
+whenever sqlerror exit sql.sqlcode
+DECLARE
+all_patches_list arr_patch_type := arr_patch_type{deploy_order};
+uninstalled_patches arr_patch_type := arr_patch_type();
+installed_patches arr_patch_type := arr_patch_type();
+BEGIN
+SELECT PATCH_NAME BULK COLLECT INTO installed_patches FROM PATCH_STATUS
+WHERE PATCH_NAME IN (select * from table(all_patches_list));
+uninstalled_patches := all_patches_list MULTISET EXCEPT installed_patches;
+FOR i IN 1..uninstalled_patches.COUNT LOOP
+DBMS_OUTPUT.PUT_LINE(uninstalled_patches(i));
+END LOOP;
+END;
+/
+exit;"""
         with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
             fp.write(query_2)
-            fp.seek(0)
+            fp.flush()
             test = self.runSqlQuery(bytes(f"@{fp.name}", 'UTF-8'))
             patches_for_install = re.findall('(.+)\n', test[0].decode('UTF-8'))
             patches_for_install.pop(-1)
