be6dc0c845d270e9d1d9a293c1fd8a005a614c69
Igor21209 <somebody73296@mail.ru>
2022-12-16 19:40:48 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -1,9 +1,8 @@
 from sys import argv
 from teamcity import Teamcity
 
-python_path, user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, oracle_user = argv
+python_path, user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, oracle_user, oracle_port = argv
 
 if __name__ == '__main__':
-    test = Teamcity(user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, oracle_user)
+    test = Teamcity(user, host, target_dir, path_to_ssh_priv_key, path_to_yaml, path_to_sqlplus, oracle_host, oracle_db, oracle_user, oracle_port)
     test.start()
-    #print(test.get_env_variable('echo $(pwd)'))
