6e2600a3d86b4c8ecbc7962307787e4fb9164ba0
Igor21209 <somebody73296@mail.ru>
2022-12-20 12:57:00 +0300

    последняя версия на 20.12.22

--- teamcity.py
+++ teamcity.py
@@ -30,11 +30,30 @@ class Teamcity:
         self.oracle_user = oracle_user
         self.oracle_port = oracle_port
 
-    def runSqlQuery(self, sqlCommand):
+    def runSqlQuery(self, sqlCommand, sqlFile=None):
+        if sqlCommand:
+            with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
+                fp.write(sqlCommand)
+                fp.flush()
+                sql = bytes(f"@{fp.name}", 'UTF-8')
+                session = Popen([f'{self.path_to_sqlplus}', '-S',
+                                 f'{self.oracle_user}/{os.environ.get("PASS")}@//{self.oracle_host}:{self.oracle_port}/{self.oracle_db}'],
+                                stdin=PIPE, stdout=PIPE,
+                                stderr=PIPE)
+                session.stdin.write(sql)
+                if session.communicate():
+                    unknown_command = re.search('unknown command', session.communicate()[0].decode('UTF-8'))
+                    if session.returncode != 0:
+                        sys.exit(f'Error while executing sql code in file {sqlCommand}')
+                    if unknown_command:
+                        sys.exit(f'Error while executing sql code in file {sqlCommand}')
+                return session.communicate()
+        else:
+            sql = bytes(f"@{sqlFile}", 'UTF-8')
         session = Popen([f'{self.path_to_sqlplus}', '-S',
                          f'{self.oracle_user}/{os.environ.get("PASS")}@//{self.oracle_host}:{self.oracle_port}/{self.oracle_db}'], stdin=PIPE, stdout=PIPE,
                         stderr=PIPE)
-        session.stdin.write(sqlCommand)
+        session.stdin.write(sql)
         if session.communicate():
             unknown_command = re.search('unknown command', session.communicate()[0].decode('UTF-8'))
             if session.returncode != 0:
@@ -54,12 +73,8 @@ class Teamcity:
     set(pathes_for_install) - set(list_of_installed_pathes_from_db) не получится использовать, т.к. порядок в таком случае не сохраняется.
     '''
     def check_patches(self, pathes_for_install, list_of_installed_pathes_from_db):
-        index_scan = 0
-        while index_scan < len(pathes_for_install):
-            if pathes_for_install[index_scan] not in (list_of_installed_pathes_from_db):
-                pathes_for_install.pop(index_scan)
-            else:
-                index_scan += 1
+        sp = set(list_of_installed_pathes_from_db)
+        pathes_for_install = [p for p in pathes_for_install if p in sp]
         return pathes_for_install
 
     '''
@@ -68,22 +83,10 @@ class Teamcity:
     порядком, указанным в файле deploy_order.yml
     '''
     def check_incorrect_order(self, commits_array, branch_array):
-        patch_index = 0
         result_compare_order = False
-        if len(branch_array) == 0:
-            return False
-        if len(commits_array) < len(branch_array):
-            return True
-        while branch_array[0] != commits_array[patch_index].branch and patch_index < len(commits_array):
-            patch_index += 1
-        for branch in branch_array:
-            if patch_index >= len(commits_array):
-                result_compare_order = True
-                return result_compare_order
-            if branch != commits_array[patch_index].branch:
-                result_compare_order = True
-                return result_compare_order
-            patch_index += 1
+        commits_list = [commit.commit for commit in commits_array]
+        if not len(commits_list) == len(branch_array):
+            result_compare_order = True
         return result_compare_order
 
     def get_current_branch(self):
@@ -97,50 +100,37 @@ WHEN NOT MATCHED THEN INSERT (PATCH_NAME, INSTALL_DATE, STATUS)
 VALUES('{patch}', current_timestamp, 'SUCCESS')
 WHEN MATCHED THEN UPDATE SET INSTALL_DATE=current_timestamp, STATUS='SUCCESS';
 exit;"""
-        with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
-           fp.write(add_to_install_patches)
-           fp.flush()
-           self.runSqlQuery(bytes(f"@{fp.name}", 'UTF-8'))
+        self.runSqlQuery(add_to_install_patches)
 
 
-    def execute_files(self, patches):
-        patches_1 = patches.get('patch')
-        patches_for_install = self.get_patches_for_install(patches_1)
+    def execute_files(self, patches_from_deploy_order):
+        patches = patches_from_deploy_order.get('patch')
+        patches_for_install = self.get_patches_for_install(patches)
         if len(patches_for_install) == 0:
             sys.exit(f'Nothing to install')
-        patches_for_install_order = self.check_patches(patches_1, patches_for_install)
-        if not (len(patches_for_install) == 1 and self.get_current_branch() == patches_for_install[0]):
-            list_of_commit_objects = self.git(patches_for_install)
-            check = self.check_incorrect_order(list_of_commit_objects, patches_for_install_order)
-            if not check:
-                for patch in list_of_commit_objects:
-                    pars = f'Patches/{patch.branch}/deploy.yml'
-                    data = self.yaml_parser(pars)
-                    sql = data.get('sql')
-                    sas = data.get('sas')
-                    if sql:
+        patches_for_install_order = self.check_patches(patches, patches_for_install)
+        list_of_commit_objects = self.git(patches_for_install)
+        check = self.check_incorrect_order(list_of_commit_objects, patches_for_install_order)
+        if not check:
+            for patch in list_of_commit_objects:
+                pars = f'Patches/{patch.branch}/deploy.yml'
+                data = self.yaml_parser(pars)
+                sql = data.get('sql')
+                sas = data.get('sas')
+                if sql:
+                    if not (len(patches_for_install) == 1 and self.get_current_branch() == patches_for_install[0]):
                         for q in sql:
                             query = self.get_commit_version(q, patch.commit)
                             self.runSqlQuery(query)
-                    if sas:
-                        for s in sas:
-                            self.ssh_copy(s, self.target_dir)
-                    self.log_patch_db_success(patch.branch)
-            else:
-                sys.exit(f"Patches order does not match commits order")
+                    else:
+                        for q in sql:
+                            self.runSqlQuery(query)
+                if sas:
+                    for s in sas:
+                        self.ssh_copy(s, self.target_dir)
+                self.log_patch_db_success(patch.branch)
         else:
-            patch = patches_for_install[0]
-            pars = f'Patches/{patch}/deploy.yml'
-            data = self.yaml_parser(pars)
-            sql = data.get('sql')
-            sas = data.get('sas')
-            if sql:
-                for q in sql:
-                    self.runSqlQuery(bytes(f"@{q}", 'UTF-8'))
-            if sas:
-                for s in sas:
-                    self.ssh_copy(s, self.target_dir)
-            self.log_patch_db_success(patch)
+            sys.exit(f"Patches order does not match commits order")
 
     def ssh_copy(self, sourse, target):
         dirs = re.split('/', sourse)
@@ -149,9 +139,9 @@ exit;"""
             if i == dirs[-1]:
                 break
             create_dirs = create_dirs + i + '/'
-        create = re.search('(SAS/).+', create_dirs)
+        create = re.search('SAS/(.+)', create_dirs)
         if create:
-            dir_for_create = create.group(0)[4:]
+            dir_for_create = create.group(1)
             dirs = subprocess.run(
                 ['ssh', '-i', f'{self.path_to_ssh_priv_key}', f'{self.user}@{self.host}', 'mkdir', '-p',
                  f'{target + dir_for_create}'])
@@ -179,7 +169,7 @@ exit;"""
         sql_exec = Popen(args=command_1,
             stdout=PIPE,
             shell=True)
-        sql_command = sql_exec.communicate()[0]
+        sql_command = sql_exec.communicate()[0].decode('UTF-8')
         return sql_command
 
     '''
@@ -213,33 +203,29 @@ exit;"""
 CREATE OR REPLACE TYPE arr_patch_type IS TABLE OF VARCHAR2(32);
 /
 exit;"""
-        with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
-            fp.write(query_1)
-            fp.flush()
-            self.runSqlQuery(bytes(f"@{fp.name}", 'UTF-8'))
+        self.runSqlQuery(query_1)
         deploy_order = str(patches).replace('[', '(').replace(']', ')').strip()
         query_2 = f"""SET SERVEROUTPUT ON
 whenever sqlerror exit sql.sqlcode
 DECLARE
-all_patches_list arr_patch_type := arr_patch_type{deploy_order};
-uninstalled_patches arr_patch_type := arr_patch_type();
-installed_patches arr_patch_type := arr_patch_type();
+  all_patches_list arr_patch_type := arr_patch_type{deploy_order};
+  uninstalled_patches arr_patch_type := arr_patch_type();
+  installed_patches arr_patch_type := arr_patch_type();
 BEGIN
-SELECT PATCH_NAME BULK COLLECT INTO installed_patches FROM PATCH_STATUS
-WHERE PATCH_NAME IN (select * from table(all_patches_list));
-uninstalled_patches := all_patches_list MULTISET EXCEPT installed_patches;
-FOR i IN 1..uninstalled_patches.COUNT LOOP
-DBMS_OUTPUT.PUT_LINE(uninstalled_patches(i));
-END LOOP;
+  SELECT PATCH_NAME BULK COLLECT INTO installed_patches FROM PATCH_STATUS
+  WHERE PATCH_NAME IN (select * from table(all_patches_list));
+  uninstalled_patches := all_patches_list MULTISET EXCEPT installed_patches;
+  DBMS_OUTPUT.PUT_LINE('START_RES');
+  FOR i IN 1..uninstalled_patches.COUNT LOOP
+    DBMS_OUTPUT.PUT_LINE(uninstalled_patches(i));
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE('FINISH_RES');
 END;
 /
 exit;"""
-        with tempfile.NamedTemporaryFile('w+', encoding='UTF-8', suffix='.sql', dir='/tmp') as fp:
-            fp.write(query_2)
-            fp.flush()
-            test = self.runSqlQuery(bytes(f"@{fp.name}", 'UTF-8'))
-            patches_for_install = re.findall('(.+)\n', test[0].decode('UTF-8'))
-            patches_for_install.pop(-1)
+        test = self.runSqlQuery(query_2)
+        all_patches = re.search('START_RES\n(.+)\nFINISH_RES', test[0].decode('UTF-8'), re.S)
+        patches_for_install = all_patches.group(1).split('\n')
         return patches_for_install
 
     def start(self):
